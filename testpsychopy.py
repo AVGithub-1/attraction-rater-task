035a66d78ed57b49741420a2643fedcaa6600a5d
Akhil Vasanth <akhilpvasanth@gmail.com>
2023-03-31 01:49:40 -0700

    added code to reformat the csv files and organized the code into functions

--- testpsychopy.py
+++ testpsychopy.py
@@ -20,7 +20,7 @@ pics_shown = [(0,0)] #will store tuples for each image; for each, index 0 has st
 
 ratings = []
 
-data = []
+data = [] # data will be stored in lists containing: image and corresponding folder, condition, face, rating
 
 
 
@@ -49,102 +49,48 @@ def arrow_choice(stim):
         return random.choice([m_vertices, r_vertices, l_vertices])
     else:
         return m_vertices
-
-for i in range(20): #loop to cycle through the images and have ppts rate each one
     
-    folder = random.choice(stim_folder) #folder of images in stimuli
-    stim_type = random.choice(stimuli) #type of image (group, blurred, or indvidual)
-    while (folder, stim_type) in pics_shown or folder in pics_shown[i] or stim_type in pics_shown[i]:
-        folder = random.choice(stim_folder) 
-        stim_type = random.choice(stimuli)
-
-    path_to_image_file = Path("/Users/akhil/psychopy_project/Stimulis") / folder / stim_type
-
-    # simply pass the image path to ImageStim to load and display:
-    image_stim = visual.ImageStim(win, image=path_to_image_file)
-
-    image_stim.draw()
-    win.flip()
-    core.wait(1)
-
-
-    # Create a blank stimulus
-    blank = visual.TextStim(win, text='')
-
-    # Display the blank stimulus
-    blank.draw()
-    win.flip()
-    core.wait(1)
-
-
-    arrow_type = arrow_choice(stim_type)
-    # Create a shape stimulus for the arrow
-    arrow = visual.ShapeStim(win, vertices=arrow_type, lineColor='white', fillColor='white')
-
-    # Display the arrow for 1 seconds
-    arrow.draw()
-    win.flip()
-    core.wait(1)
-
-    ratingScale = visual.RatingScale(
-        win, 
-        labels = ['unattractive', 'attractive'], 
-        marker = 'circle',
-        scale = None,
-        low = 1, high = 1000,
-        showValue = False,
-        acceptText = "click to accept"
-        )
-    
-    while ratingScale.noResponse:
-        ratingScale.draw()
-        win.flip()
-
-
-    rating = ratingScale.getRating()
-    decisionTime = ratingScale.getRT()
-    choiceHistory = ratingScale.getHistory()
-
-    #update list of ratings and list of pictures already shown
-    pics_shown.append((folder, stim_type))
-    ratings.append(rating)
-
-del pics_shown[0]
-
-
-
-def update_data(folder, stim_type, rating):
+def update_data(folder, stim_type, rating, arrow_type):
     """
     this function uses regular expressions and string concatenation to update the data list with
     specificly-formatted information about each trial
+
+    Parameters:
+    
     """
-    image = stim_type - ".jpg" 
+    condition = stim_type.replace('.jpg', '')
+    condition = condition.replace(' ', '')
 
     pattern = r'[0-9]'
 
-    image = re.sub(pattern, '', image)
+    condition = re.sub(pattern, '', condition)
 
-    data.append([image, stim_type, rating])
+    image = condition + "_" + folder
 
+    face = folder 
+    #initial value for face is the folder, next the specific face will be appended
+    #the specific face will be denoted, from left to right, as a, b, or c
 
+    if condition == "Blurred" or condition == 'Group':
+        if arrow_type == l_vertices:
+            face += 'a'
+        elif arrow_type == m_vertices:
+            face += 'b'
+        elif arrow_type == r_vertices:
+            face += 'c'
+    
+    if condition == "Individual":
+        if stim_type.__contains__('1'):
+            face += 'a'
+        elif stim_type.__contains__('2'):
+            face += 'b'
+        elif stim_type.__contains__('3'):
+            face += 'c'
 
-#make code that saves answers to a csv file
-#make it so that a new one is made every time you run the code
+    data.append([image, condition, face, rating])
+    
 
-csv_files = list(Path("csv_files").iterdir())
-header = ["Image", "Condition", "Face", "Subject Rating"]
-
-if len(csv_files) == 0:
-    with open(Path("csv_files")/'results0.csv', 'w', encoding='UTF8', newline= '') as f:
-        writer = csv.writer(f)
-
-        writer.writerow(header)
-
-        # write multiple rows
-        writer.writerows(data)
-
-else:
-    index = str(len(csv_files))
+def new_csv(header, data, index):
     with open(Path("csv_files")/('results'+index+'.csv'), 'w', encoding='UTF8', newline= '') as f:
         writer = csv.writer(f)
 
@@ -153,6 +99,104 @@ else:
         # write multiple rows
         writer.writerows(data)
 
+
+def run_experiment(iterations):
+
+    for i in range(iterations): #loop to cycle through the images and have ppts rate each one
+        
+        folder = random.choice(stim_folder) #folder of images in stimuli
+        stim_type = random.choice(stimuli) #type of image (group, blurred, or indvidual)
+        while (folder, stim_type) in pics_shown or folder in pics_shown[i] or stim_type in pics_shown[i]:
+            folder = random.choice(stim_folder) 
+            stim_type = random.choice(stimuli)
+
+        #gets the path of the specific stimuli
+        #Note: the string in Path() will have to be changed to the specific directory this project is in on your
+        #computer
+        path_to_image_file = Path("/Users/akhil/psychopy_project/Stimulis") / folder / stim_type
+
+        # simply pass the image path to ImageStim to load and display:
+        image_stim = visual.ImageStim(win, image=path_to_image_file)
+
+        image_stim.draw()
+        win.flip()
+        core.wait(1)
+
+
+        # Create a blank stimulus as a buffer between the stimuli and the arrow
+        # blank = visual.TextStim(win, text='')
+
+        # # Display the blank stimulus
+        # blank.draw()
+        # win.flip()
+        # core.wait(1)
+
+
+        arrow_type = arrow_choice(stim_type)
+        # Create a shape stimulus for the arrow
+        arrow = visual.ShapeStim(win, vertices=arrow_type, lineColor='white', fillColor='white')
+
+        # Display the arrow for 1 seconds
+        arrow.draw()
+        win.flip()
+        core.wait(1)
+
+        ratingScale = visual.RatingScale(
+            win, 
+            labels = ['unattractive', 'attractive'], 
+            marker = 'circle',
+            scale = None,
+            low = 1, high = 1000,
+            showValue = False,
+            acceptText = "click to accept"
+            )
+        
+        while ratingScale.noResponse:
+            ratingScale.draw()
+            win.flip()
+
+
+        rating = ratingScale.getRating()
+        decisionTime = ratingScale.getRT()
+        choiceHistory = ratingScale.getHistory()
+
+        #update list of ratings and list of pictures already shown
+        pics_shown.append((folder, stim_type))
+        ratings.append(rating)
+
+        update_data(folder, stim_type, rating, arrow_type)
+
+    del pics_shown[0]
+
+    #this part of the function makes a new csv file and puts all of the subject's data in it
+    csv_files = list(Path("csv_files").iterdir())
+    header = ["Image", "Condition", "Face", "Subject Rating"]
+
+    if len(csv_files) == 0:
+        with open(Path("csv_files")/'results0.csv', 'w', encoding='UTF8', newline= '') as f:
+            writer = csv.writer(f)
+
+            writer.writerow(header)
+
+            # write multiple rows
+            writer.writerows(data)
+
+    else:
+        index = str(len(csv_files))
+        directory = Path("/Users/akhil/psychopy_project/csv_files") 
+
+        # Specify the filename of the CSV file
+        filename = "results" + index + ".csv"
+
+        # Check if the file exists in the directory
+        csv_file_path = directory / filename
+
+        if csv_file_path.is_file():
+            new_csv(header, data, index)
+
+        else:
+            index = str(len(csv_files)+1)
+            new_csv(header, data, index)
 
 # make a loop, have it loop through each picture and for each picture, give a slider to rate attractiveness
 # and a next button to go to the next image
@@ -172,3 +216,6 @@ else:
 # you have all these variables(image name, etc.) that are written to a csv file
 
 #demographic info: age, sex, sexual orientation
+
+
+run_experiment(4)
